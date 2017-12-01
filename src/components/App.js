2f1acc3b7756af4122a0c3f302f2a16b949fffee
zpaul0 <z_paul@sina.cn>
2017-11-30 23:20:29 -0700

    add logout func

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import { Header } from './Header';
-import { Main } from './Main'
-import { TOKEN_KEY } from "../constants"
+import React, {Component} from 'react';
+import {Header} from './Header';
+import {Main} from './Main'
+import {TOKEN_KEY} from "../constants"
 import '../styles/App.css';
 
 class App extends Component {
     state = {
-        isLoggedIn: false,
+        isLoggedIn: !!localStorage.getItem(TOKEN_KEY),
     }
     handleLogin = (token) => {
         localStorage.setItem(TOKEN_KEY, token);
-        this.setState({isLoggedIn: true });
-}
+        this.setState({isLoggedIn: true});
+    }
+    handleLogout = () => {
+        this.setState({isLoggedIn: false});
+        localStorage.removeItem(TOKEN_KEY);
+    }
+
+
     render() {
         return (
             <div className="App">
-                <Header isLoggedIn={this.state.isLoggedIn} />
+                <Header isLoggedIn={this.state.isLoggedIn} handleLogout={this.handleLogout}/>
                 <Main isLoggedIn={this.state.isLoggedIn} handleLogin={this.handleLogin}/>
             </div>
         );
