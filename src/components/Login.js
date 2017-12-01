f952fa20dcca1ea67ec333eef4d5794e1f955169
zpaul0 <z_paul@sina.cn>
2017-11-30 22:08:53 -0700

    fix Urls

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,14 +4,10 @@ import $ from 'jquery';
 import { Form, Icon, Input, Button, message } from 'antd';
 import { Link } from 'react-router-dom';
 import { API_ROOT } from '../constants';
-import PropTypes from 'prop-types';
 
 const FormItem = Form.Item;
 
 class NormalLoginForm extends React.Component {
-    static propTypes = {
-        handleLogin: PropTypes.func.isRequired,
-    }
 
     handleSubmit = (e) => {
         e.preventDefault();
@@ -26,7 +22,7 @@ class NormalLoginForm extends React.Component {
                         password: values.password,
                     })
                 }).then((response) => {
-                    this.props.handleLogin(response);
+                    message.success(response);
                 }, (error) => {
                     message.error(error.responseText);
                 }).catch((error) => {
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { Register } from './Register';
 import { Login } from './Login';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect} from 'react-router-dom';
+import {Home } from './Home'
 
 export class Main extends React.Component {
+    getLogin = () => {
+        return this.props.isLoggedIn ? <Redirect to="/home"/> : <Login/>;
+    }
+    getHome = () => {
+        return this.props.isLoggedIn ? <Home/> : <Redirect to="/Login"/>;
+    }
+    getRoot = () => {
+        return <Redirect to="/Login"/>
+    }
     render() {
         return (
             <div className="main">
                 <Switch>
-                    <Route path="/login" component={Login}/>
-                    <Route path="/register" component={Register}/>
-                    <Route path="/Login" component={Login}/>
+                    <Route path="/Login" render={this.getRoot()}/>
+                    <Route path="/register" render={this.getLogin}/>
+                    <Route path="/Login" component={Register}/>
+                    <Route path="/Home" render={this.getHome}/>
                     <Route component={Login}/>
                 </Switch>
             </div>
